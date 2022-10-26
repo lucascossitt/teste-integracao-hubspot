3275cabf213ec967167b3d07eb38d2654557dc8b
lucascossitt <lucascossitt@hotmail.com>
2022-10-26 10:36:34 -0300

    Adicionando regra de negócio para email do contato

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const getSpreadsheetRows = require('./services/Google/Spreadsheet/getSpreadsheetRows')
 const createContactHubspot = require('./services/Hubspot/createContact')
 const Sleep = require('./models/Sleep')
+const publicEmailDomainsList = require('./models/publicEmailDomainsList')
 
 getSpreadsheetRows(process.env.GOOGLE_SPREADSHEET_ID).then(async rows => {
 
     for await (let row of rows) {
         try {
+            if (publicEmailDomainsList.includes(row.email)) continue;
+
             await Sleep(1000)
             await createContactHubspot(row)
                 .then(() => console.log(`(${rows.indexOf(row) + 1}/${rows.length}) Contato "${row.nomeCompleto}" criado com sucesso`))
---
+++ src/models/publicEmailDomainsList.js
@@ -0,0 +1,10 @@
+module.exports = [
+    "gmail.com",
+    "yahoo.com",
+    "hotmail.com",
+    "aol.com",
+    "outlook.com",
+    "live.com",
+    "uol.com.br",
+    "icloud.com"
+]
